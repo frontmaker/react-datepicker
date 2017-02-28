ad22da71ea68c15eeb314b88f303e8afba643ed9
frontmaker <Djeka345@gmail.com>
2017-02-28 20:42:00 +0300

    initialize Weeks component

--- src/components/Weeks.jsx
+++ src/components/Weeks.jsx
@@ -3,26 +3,26 @@ import React from 'react';
 
 const Week = (props) => {
 
-    var days = [],
+    let days = [],
         date = props.date,
         month = props.month;
 
 
-    for (var i = 0; i < 7; i++) {
-        var day = {
+    for (let i = 0; i < 7; i++) {
+        let day = {
             name: date.format("dd").substring(0, 1),
             number: date.date(),
             isCurrentMonth: date.month() === month.month(),
             isToday: date.isSame(new Date(), "day"),
             date: date
         };
-        days.push(<span key={day.date.toString()}>{day.number}</span>);
+        days.push(<span className="calendar-days__day" key={day.date.toString()}>{day.number}</span>);
         date = date.clone();
         date.add(1, "d");
 
     }
 
-    return <div className="week">
+    return <div className="calendar-days">
         {days}
     </div>
 }
@@ -30,19 +30,29 @@ const Week = (props) => {
 
 const Weeks = (props) => {
 
-    var weeks = [],
+    let weeks = [],
         done = false,
-        date = props.date.clone().startOf("month").add("w" - 1).day("Sunday"),
+        date = props.date.clone().startOf("month").weekday(0),
         monthIndex = date.month(),
         count = 0;
 
-    while (!done) {
+    for (let i = 0; i < 5; i++) {
 
-        weeks.push(<Week key={date.toString()} date={date.clone()} month={props.date} />);
-        date.add(1, "w");
-        done = count++ > 2 && monthIndex !== date.month();
-        monthIndex = date.month();
+        if (monthIndex === date.month()) {
+            weeks.push(<Week key={date.toString()} date={date.clone()} month={props.date} />);
+            date.add(1, "w");
+            monthIndex = date.month();
+        }
     }
+
+
+    // while (!done) {
+    //
+    //     weeks.push(<Week key={date.toString()} date={date.clone()} month={props.date} />);
+    //     date.add(1, "w");
+    //     done = count++ > 2 && monthIndex !== date.month();
+    //     monthIndex = date.month();
+    // }
 
 
     return(
